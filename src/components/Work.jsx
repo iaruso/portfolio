c2997dfc67b0980603cebba574c6ce74569e1739
iaruso <iaruso.me@gmail.com>
2023-08-07 21:09:30 +0100

    Minor syntax fix

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -10,8 +10,8 @@ const Work = () => {
         <div className="works-content">
           <Link to="/work/circuit-rush" className="card">
             <img
-              srcSet="./static/circuit-rush-cover.png 1024w,
-                      ./static/circuit-rush-cover-min.png 768w"
+              srcset="./static/circuit-rush-cover-min.png 768w,
+											./static/circuit-rush.png 1024w"
               sizes="(max-width: 768px) 768px, 1024px"
               src="./static/circuit-rush-cover.png"
               alt="Circuit Rush Cover"
@@ -19,8 +19,8 @@ const Work = () => {
           </Link>
           <Link to="/work/movera" className="card">
             <img
-              srcSet="./static/movera-cover.png 1024w,
-                      ./static/movera-cover-min.png 768w"
+              srcset="./static/movera-cover-min.png 768w,
+											./static/movera-cover.png 1024w"
               sizes="(max-width: 768px) 768px, 1024px"
               src="./static/movera-cover.png"
               alt="Movera Cover"
@@ -28,8 +28,8 @@ const Work = () => {
           </Link>
           <Link to="/work/expns" className="card">
             <img
-              srcSet="./static/expns-cover.png 1024w,
-                      ./static/expns-cover-min.png 768w"
+              srcset="./static/expns-cover-min.png 768w,
+											./static/expns-cover.png 1024w"
               sizes="(max-width: 768px) 768px, 1024px"
               src="./static/expns-cover.png"
               alt="Expns Cover"
