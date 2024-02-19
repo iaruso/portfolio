7b6fd68ad2df1cdd5451ed78eaf70eb1228d122f
Yaraslau <iaruso.me@gmail.com>
2024-02-19 18:56:06 +0000

    Updating website content

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-import ScrollToTop from "./components/ScrollToTop";
-import Main from './pages/Main';
-import CircuitRush from './pages/CircuitRush';
-import Movera from './pages/Movera';
-import Expns from './pages/Expns';
 
-const router = [
-  { path: '/', element: <Main/> },
-  { path: '/work/circuit-rush', element: <CircuitRush/> },
-  { path: '/work/movera', element: <Movera/>},
-  { path: '/work/expns', element: <Expns/> },
-	{ path: '*', element: <Main/> },
-];
+
 
 function App() {
   return (
-    <Router>
-      <div className="app">
-        <ScrollToTop>
-					<Routes>
-          {router.map(({ path, element }) => (
-            <Route key={path} exact path={path} element={element} />
-          ))}
-					</Routes>
-        </ScrollToTop>
-      </div>
-    </Router>
+    <>
+      <p>Coming Soon</p>
+    </>
   );
 }
 
